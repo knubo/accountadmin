3e58107483eb7c95b2811b6ec50bd9fcb7e09d80
knutbo <knutbo@f3acff23-2fee-298d-aad2-0ed59a7633b3>
2010-04-14 07:11:38 +0000

    Work.

--- trunk/RegnskapAdmin/src/no/knubo/accounting/admin/client/AdminGWT.java
+++ trunk/RegnskapAdmin/src/no/knubo/accounting/admin/client/AdminGWT.java
@@ -1,21 +1,13 @@
 package no.knubo.accounting.admin.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.DockPanel;
-import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
 public class AdminGWT implements EntryPoint {
-    public void onModuleLoad() {
-        DockPanel dp = new DockPanel();
 
-        HorizontalPanel hp = new HorizontalPanel();
-        
-        dp.add(hp, DockPanel.NORTH);
-        
-        RootPanel.get().add(dp);
+    public void onModuleLoad() {
+        RootPanel.get().add(new HTML("Hei verden"));
     }
+
 }
