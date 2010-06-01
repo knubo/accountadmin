753661e103c6bae406fa879450d4ee949ee41c0d
knutbo <knutbo@f3acff23-2fee-298d-aad2-0ed59a7633b3>
2010-06-01 21:01:09 +0000

    Send mail when new question added.

---
+++ RegnskapAdmin/wikka/plugins/actions/frnewquestion.php
@@ -0,0 +1,29 @@
+<?php
+
+$detail = array_key_exists("detail", $_REQUEST) ? $_REQUEST["detail"] : "";
+$title = array_key_exists("title", $_REQUEST) ? $_REQUEST["title"] : "";
+
+$user = 0;
+if($this->GetUser()) {
+    $user = $this->GetUser();
+    $user = $user["name"];
+}
+
+$eol="\r\n";
+$sender = "question@frittregnskap.no";
+$headers .= "From: ".$sender.$eol;
+$headers .= "Content-Type:text/plain;";
+$headers .= "Message-ID: <".time()."-".$sender.">".$eol;
+$headers .= "X-Mailer: PHP v".phpversion().$eol;          // These two to help avoid spam-filters
+
+$subject = "New question";
+$email = "question@frittregnskap.no";
+
+$msg = "Nytt sp¿rsmal: $title\nDetaljer: $detail\nInnlagt av:".json_encode($user);
+
+mail($email, $subject, $msg, $headers);
+
+
+header("Location: http://www.frittregnskap.no/wakka/ThanksQuestionAdded");
+
+?>
