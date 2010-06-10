f4f24cc44e5907027802fd93dceb68b5027a99ee
knutbo <knutbo@f3acff23-2fee-298d-aad2-0ed59a7633b3>
2010-06-10 19:23:53 +0000

    More style + Norwegian.

--- RegnskapAdmin/wikka/actions/historylink.php
+++ RegnskapAdmin/wikka/actions/historylink.php
@@ -4,7 +4,7 @@
  */
 //i18n
 if (!defined('HISTORYLINK_TEXT')) define('HISTORYLINK_TEXT', '[Historikk]');
-if (!defined('HISTORYLINK_TITLE')) define('HISTORYLINK_TITLE', 'Click to view recent edits to this page');
+if (!defined('HISTORYLINK_TITLE')) define('HISTORYLINK_TITLE', 'Klikk for &aring; se siste redigeringer av denne siden');
 
 echo '<a href="'.$this->Href('history').'" title="'.HISTORYLINK_TITLE.'">'.HISTORYLINK_TEXT.'</a>'."\n";
 ?>
