e1b5c13b7b525b7c694041688717faae936994e8
knutbo <knutbo@f3acff23-2fee-298d-aad2-0ed59a7633b3>
2010-06-11 18:21:19 +0000

    - Some style - Capcha for new users.

--- RegnskapAdmin/wikka/actions/revisionlink.php
+++ RegnskapAdmin/wikka/actions/revisionlink.php
@@ -3,8 +3,8 @@
  * Revision link menulet
  */
 //i18n
-if (!defined('REVISIONLINK_TITLE')) define('REVISIONLINK_TITLE', 'Click to view recent revisions list for this page');
-if (!defined('REVISIONFEEDLINK_TITLE')) define('REVISIONFEEDLINK_TITLE', 'Click to display a feed with the latest revisions to this page');
+if (!defined('REVISIONLINK_TITLE')) define('REVISIONLINK_TITLE', 'Klikk for &aring; se nylige revisjoner av denne siden');
+if (!defined('REVISIONFEEDLINK_TITLE')) define('REVISIONFEEDLINK_TITLE', 'Klikk for &aring; f&aring; en str&oslash;m av siste revisjoner av denne siden');
 
 echo $this->GetPageTime() ? '<a class="datetime" href="'.$this->Href('revisions').'" title="'.REVISIONLINK_TITLE.'">'.$this->GetPageTime().'</a> <a href="'.$this->Href('revisions.xml').'" title="'.REVISIONFEEDLINK_TITLE.'"><img src="images/feed.png" class="icon" width="14" height="14" alt="feed icon" /></a>' : '';
 ?>
