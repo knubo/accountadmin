a7cd7fa85d58bbae2125528f0f8d1653a8c31e65
knutbo <knutbo@f3acff23-2fee-298d-aad2-0ed59a7633b3>
2010-06-12 09:14:00 +0000

    Styling and help for signup application.

--- RegnskapAdmin/wikka/plugins/actions/frsignup.php
+++ RegnskapAdmin/wikka/plugins/actions/frsignup.php
@@ -1,4 +1,9 @@
+<?php 
 
-<script language="javascript"
-	src="/frsignup/no.knubo.accounting.admin.SignupGWT.nocache.js"></script>
-<div id="signupapp"><!-- In here --></div>
+if($this->GetUser()) {
+    echo "<script language=\"javascript\"";
+	echo "src=\"/frsignup/no.knubo.accounting.admin.SignupGWT.nocache.js\"></script>";
+    echo "<div id=\"signupapp\"><!-- In here --></div>";
+}
+
+?>
