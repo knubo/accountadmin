03bbc67b9b5578aa0e0651526ba267fb237bf798
knutbo <knutbo@f3acff23-2fee-298d-aad2-0ed59a7633b3>
2010-07-02 11:52:09 +0000

    Several email addresses on one user.

--- RegnskapAdminClient/src/no/knubo/accounting/admin/client/validation/EmailValidator.java
+++ RegnskapAdminClient/src/no/knubo/accounting/admin/client/validation/EmailValidator.java
@@ -1,5 +1,7 @@
 package no.knubo.accounting.admin.client.validation;
 
+import com.google.gwt.user.client.Window;
+
 public class EmailValidator extends ValidatorBase {
 
     public EmailValidator(String errorText) {
@@ -14,6 +16,21 @@ public class EmailValidator extends ValidatorBase {
             return true;
         }
 
+        String[] severalEmails = email.split("\\,");
+
+        if(severalEmails.length > 1) {
+            for (String string : severalEmails) {
+                if(!doValidation(string)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        return doValidation(email);
+    }
+
+    private boolean doValidation(String email) {
         int apos = email.indexOf('@');
 
         /* One char before and not last sign, not 2+ of them. */
