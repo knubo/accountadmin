9dbed8e96e1e8cb6a96c43ed68137f534897abab
knutbo <knutbo@f3acff23-2fee-298d-aad2-0ed59a7633b3>
2010-07-08 22:29:54 +0000

    Make links in submenu somewhat more visual.

--- RegnskapAdmin/wikka/templates/indigo/js/installs.js
+++ RegnskapAdmin/wikka/templates/indigo/js/installs.js
@@ -1,28 +1,29 @@
 function showInstalls(event) {
-    var pos = $("#mittregnskap").offset();  
-    var height = $("#mittregnskap").outerHeight();
-    $("#installs").css( { position: 'absolute', "left": (pos.left) + "px", "top":(pos.top+height) + "px" } );
-    $("#installs").show();
+	var pos = $("#mittregnskap").offset();
+	var height = $("#mittregnskap").outerHeight();
+	$("#installs").css( {
+		position : 'absolute',
+		"left" : (pos.left) + "px",
+		"top" : (pos.top + height) + "px"
+	});
+	$("#installs").show();
 }
 
 function hideInstalls(event) {
-    $("#installs").delay(100).hide();
-    $("#installs").removeClass("hovering");
+	$("#installs").delay(100).hide();
+	$("#installs").removeClass("hovering");
 }
 
 function hideInstallsNow(event) {
-    $("#installs").hide();
-    $("#mittregnskap").removeClass("hovering");
+	$("#installs").hide();
+	$("#mittregnskap").removeClass("hovering");
 }
-
 
 function showInstallsNow(event) {
-    $("#installs").stop(1);
-    $("#installs").show();
-    $("#mittregnskap").addClass("hovering");
+	$("#installs").stop(1);
+	$("#installs").show();
+	$("#mittregnskap").addClass("hovering");
 }
-
-
 
 $("#installs").mouseover(showInstallsNow);
 $('#installs').mouseleave(hideInstallsNow);
